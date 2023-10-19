cfff937d5c97ebaec8b833d24ceb16ca2d7884ad
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-10-19 22:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,77 @@
-//your code here
- var ans = document.getElementById('ans');
-        var input = document.getElementById('input');
+//your JS code here. If required.
+let displayxyz = document.getElementById("display")
 
-        function addNum(value){
-            input.value += ""+value;
+let buttonsxyz = Array.from(document.getElementsByClassName("btn"))
+
+function firstFunctionxyz(btnxyz){
+    btnxyz.addEventListener("click",(xyz)=>{
+        switch(xyz.target.innerText){
+            case 'C':
+                displayxyz.innerText = ""
+                break
+            case '←':
+                displayxyz.innerText = displayxyz.innerText.slice(0,-1)
+                break
+            case '=':
+                try{
+                    displayxyz.innerText = eval(displayxyz.innerText)
+                    // djfbdj
+                    // diuhjdbjh
+                }catch{
+                    displayxyz.innerText="Error"
+                }
+                break
+            default:
+                displayxyz.innerText = display.innerText + xyz.target.innerText
+                break
         }
-        function addOp(op){
-            input.value += op;
+    })
+}
+
+function isOpxyz(opxyz){
+    if(opxyz==="+" || opxyz==='-' || opxyz==='/' || opxyz==='*')
+        return true
+    return false
+}
+function clickingxyz(clickedxyz){
+    if(displayxyz.innerText===""){
+        if(clickedxyz!='0'){
+            displayxyz.innerText = displayxyz.innerText+clickedxyz
         }
-        function handleClear(){
-            input.value = "";
+    }
+    else{
+        var innerTextxyz = displayxyz.innerText
+        var lenxyz = innerTextxyz.length
+
+        var lastCharxyz = innerTextxyz[lenxyz-1]
+
+        if(isOpxyz(lastCharxyz)===true){
+            if(clickedxyz!='0'){
+                if(isOpxyz(clickedxyz)===true){
+                    displayxyz.innerText = displayxyz.innerText.slice(0,-1)
+                }
+                displayxyz.innerText = displayxyz.innerText+clickedxyz
+
+            }
+        }else{
+            displayxyz.innerText = displayxyz.innerText+clickedxyz
         }
-        function handleClick(){
-            let res = eval(input.value)
-            input.value = res;
-        }
+
+    }
+}
+
+function evalxyz(){
+    try{
+        displayxyz.innerText = eval(displayxyz.innerText)
+    }catch(err){
+        displayxyz.innerText="Error"
+    }
+}
+
+function clearxyz(){
+    displayxyz.innerText = ""
+}
+
+function backxyz(){
+    displayxyz.innerText = displayxyz.innerText.slice(0,-1)
+}
